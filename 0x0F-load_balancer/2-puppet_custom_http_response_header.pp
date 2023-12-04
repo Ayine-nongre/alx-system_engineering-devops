e5cd894c38ac3a1b3731ab46fa83781977c72ec2
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-04 11:58:40 +0000

    Adding task 1 and 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,38 @@
+package {'nginx':
+  ensure => installed
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+        listen          80 default_server;
+        listen          [::]:80 default_server;
+
+        root            /var/www/html;
+        index           index.html index.htm index.nginx-debian.html;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=TfgBHC5gvTI;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+        	root /var/www/html/;
+        	internal;
+    	}
+}
+",
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
