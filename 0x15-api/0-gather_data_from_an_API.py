ca2db939823db0ee5fce39b773a63eb5792784e6
Ayine-nongre <eugeneatinbire@gmail.com>
2024-01-03 10:05:00 +0000

    Adding 0 again

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,8 +10,10 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    details = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
-    data = requests.get('https://jsonplaceholder.typicode.com/users/'+ argv[1] +'/todos/')
+    details = requests.get('https://jsonplaceholder.typicode.com/users/'
+                           + argv[1])
+    data = requests.get('https://jsonplaceholder.typicode.com/users/'
+                        + argv[1] + '/todos/')
     data = data.json()
     name = details.json()['name']
     completed = 0
@@ -20,7 +22,9 @@ if __name__ == '__main__':
         if tasks.get('completed') is True:
             completed = completed + 1
 
-    print("Employee {} is done with tasks({}/{}):".format(name, completed, len(data)))
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, completed, len(data)))
+
     for tasks in data:
         if tasks.get('completed') is True:
             print("\t" + tasks.get("title"))
